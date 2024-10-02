5f9c0eb0b427abc8b57056715173a34164626611
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-10-02 15:18:50 +0600

    feat: Add empty cart design

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const productDetails = [
   {
     id: 2,
     title: "Apple Headphone M3",
+    size: "Small",
+    color: "Maroon",
     image: "./assets/headphone2.jpeg",
     price: 30,
     quantity: 1,
@@ -21,6 +23,8 @@ const productDetails = [
   {
     id: 3,
     title: "Product 2",
+    size: "Small",
+    color: "Maroon",
     image: "./assets/headphone.jpg",
     price: 50,
     quantity: 1,
@@ -104,7 +108,7 @@ function generateProductDetailsHTML(products) {
 // Set cart details in DOM if the cart is empty
 function setInitialEmptyCartDOM() {
   return `
-    <div class="cart-details-content">
+    <div class="empty-cart-details-content">
         <div class="cart-header">
             <div class="cart-header-left">
                 <div class="cart-icon">
@@ -124,8 +128,9 @@ function setInitialEmptyCartDOM() {
             <p>Your cart is currently empty.</p>
             <div class="empty-cart-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                  </svg>
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                </svg>
+
             </div>
         </div>
     </div>
